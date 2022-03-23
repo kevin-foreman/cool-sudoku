08ccf83b8f6e19dec7e9f19c277ed079d5fcb431
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-03-23 17:26:05 -0500

    refactored code and modified comments and pseudo code to match current state of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,3 @@
-// add functionality to the start button to call the puzzle demensions when clicked
-
-// call the numbers based on how many times....
-
-// make a timer so the user only has a certain amount of time to complete the puzzle
-
-//  end the user's turn if time runs out
-
-// add data-cell="1" ,, data-cell="2" ,, data-cell="3" etc... to html???
-// trim the html down to just 9 columns and nine rows and use JS to dynamically add the content from the API call
-// if we go with hard coded cells (which is fine) consider adding "contenteditable="true" so the user can input text
-// would just have to add some kind of listener to capture what they enter
-
 // create querySelectors to represent the areas we want to react to user input
 var puzzleButtonEl = document.querySelector("#btn");
 var puzzleContainerEl = document.querySelector("table");
@@ -25,9 +12,9 @@ var rowSevenCellsEl = document.querySelector("#seven");
 var rowEightCellsEl = document.querySelector("#eight");
 var timeEl = document.querySelector("#countdown");
 
+// future functionality for the solve button
 // var solveButtonEl = document.querySelector("#btn");
 
-// establish the baseline grid for the numbers
 
 // Use an empty array and add numbers dynamically with the response from the API
 var userInitGrid = [];
@@ -35,38 +22,36 @@ var solvedGrid =[];
 
 
 // pull a puzzle from the API
-// set the value of the global userInitGrid array to the newly pulled array
+// set the value of the global array to the newly array fetched from the API
 // this will happen on click of "Start"
-// conditional to ... 
 function generatePuzzle() {
-  var apiUrl = "https://sugoku.herokuapp.com/board?difficulty=medium";
-  fetch(apiUrl).then(function (response) {
+// future functionality will replace the "medium" difficulty puzzle call
+// here we call for a medium puzzle
+// future developmemt will include different difficulty buttons and change the API call
+var apiUrl = "https://sugoku.herokuapp.com/board?difficulty=medium";
+fetch(apiUrl).then(function (response) {
     if (response.ok) {
-      response.json().then(function (data) {
+    response.json().then(function (data) {
         // displayPuzzle(data, array);
         userInitGrid = data.board;
         solvedGrid = data.solved;
         generateGrid();
+    });
+    };
+});
+};
 
-        console.log(userInitGrid);
-      });
-    }
-    // generatePuzzle();
-  });
-}
-console.log(userInitGrid);
-// function to dynamically generate number fields
-
-
+// function to dynamically add the array to the page
 // loop through each array of 9 numbers
 // then loop through all 9 arrays
 // inside the first for loop handle the individual rows
 // inside the second for loop will have the actual td content
-// check
-
+// check whether there are remaining characters to add
+// this function takes in the global array and iterates through it
+// next as it iterates through it, it adds the numbers to the page
 function generateGrid(count, values) {
   // console.log("click");
-  for (i = 0; i < userInitGrid.length; i++) {
+    for (i = 0; i < userInitGrid.length; i++) {
     for (j = 0; j < userInitGrid[i].length; j++) {
     console.log(userInitGrid[i][j]);
     document.querySelector('tr:nth-child('+(i + 1) + ') > td:nth-child('+(j + 1) + ')').textContent = '';
@@ -74,19 +59,6 @@ function generateGrid(count, values) {
         document.querySelector('tr:nth-child(' + (i + 1) + ') > td:nth-child(' + (j + 1) + ')').textContent = userInitGrid[i][j];
     };
 };
-};
-
-
-// function to display the puzzle to the grid
-function displayPuzzle(grid, data) {
-    if (userInitGrid.length === 0) {
-        puzzleContainerEl.textContent = "No puzzle found.";
-
-    };
-    function puzzlePush() {
-        userInitGrid.push
-    } 
-    // console.log(tr, td);
 };
 
 // Add function to listen for what number a user puts into a cell
@@ -97,20 +69,19 @@ function addNumber() {
 
 function buttonClickHandler(event) {
   // use this function to do local storage fetch
-  var cellClick = event.target.getAttribute("data-cell");
-  // console.log(cell);
-  if (cell) {
+    var cellClick = event.target.getAttribute("data-cell");
+    if (cell) {
     puzzle(grid);
-
     grid.textContent = "";
-  }
-}
+};
+};
 
-// how to handle local storage
-// save the matrix to a global variable
-// refresh will go to local storage instead of the fetch
-// on initial page load, grab information from local storage
+// how to handle local storage?
+// save the global variable after the API pull to local storage
+// page refresh will go to local storage instead of the fetch
+// on initial page load, grab information from local storage, if any exists
 
+// event listeners to call the functions when a user clicks certain areas
 puzzleButtonEl.addEventListener("click", generatePuzzle);
 rowZeroCellsEl.addEventListener("click", addNumber);
 rowOneCellsEl.addEventListener("click", addNumber);
@@ -125,12 +96,11 @@ rowEightCellsEl.addEventListener("click", addNumber);
 
 // once the button is clicked, start a timer
 // clicking start btn 
-
 function startTimer() {
 var duration = moment.duration({
     'minutes': 5,
     'seconds': 00
-  });
+});
 
 var timestamp = new Date(0, 0, 0, 2, 10, 30);
 var interval = 1;
@@ -145,14 +115,13 @@ sec -= 1;
 if (min < 0) return clearInterval(timer);
 if (min < 10 && min.length != 2) min = '0' + min;
 if (sec < 0 && min != 0) {
-  min -= 1;
-  sec = 59;
+    min -= 1;
+    sec = 59;
 } else if (sec < 10 && sec.length != 2) sec = '0' + sec;
 
 timeEl.textContent= (min + ':' + sec);
 if (min === 0 && sec === 0)
-  clearInterval(timer);
+    clearInterval(timer);
 
 }, 1000);
 };
-
